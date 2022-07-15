1c1bddc34f32f0ba93c04212a53038466a0160e4
xafzz <15553626266@163.com>
2022-07-15 17:39:16 +0800

    Update index.ts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,2 +1,7 @@
+import Vue from 'vue'
 export { setupAssets } from '@/plugins/assets';
 export { setupElementPlus } from '@/plugins/element-plus';
+
+
+Vue.use(setupAssets)
+   .use(setupElementPlus)
